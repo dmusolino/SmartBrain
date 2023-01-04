60758250a558880d1cc7714813dd861fae4d1105
Damian <dmusolino@ymail.com>
2023-01-04 15:12:55 -0300

    react app file set up and navigation component

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navigation from './Components/navigation/navigation';
+
+function App() {
+  return (
+    <div className="App">
+   <Navigation />
+   {/* <Logo />
+   <ImageLinkForm />
+   <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
